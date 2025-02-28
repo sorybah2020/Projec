d544e7bfc5eb68a0f9c571c209507c7323b7f177
Dorisa Shehi <doris.shehi2@gmail.com>
2025-02-27 23:21:29 -0500

    Edit delete style functionality added

--- frontend/src/pages/TransactionsTable.jsx
+++ frontend/src/pages/TransactionsTable.jsx
@@ -9,6 +9,11 @@ import PropTypes from "prop-types";
 const TransactionsTable = ({ authId = "67c0ffcf02a6253bfbd4cdbb" }) => {
   const [transactions, setTransactions] = useState([]);
   const [transLoading, setTransLoading] = useState(true);
+  const [actLink, setActLink] = useState({
+    edit: false,
+    delete: false,
+  });
+  const [editIds, setEditids] = useState([]);
 
   useEffect(() => {
     const fetchTransactions = async () => {
@@ -56,6 +61,23 @@ const TransactionsTable = ({ authId = "67c0ffcf02a6253bfbd4cdbb" }) => {
     setIsOpen({ ...modalIsOpen, [modalName]: true });
   }
 
+  const handleCheck = (id) => {
+    if (editIds.includes(id)) {
+      setEditids(editIds.filter((item) => item !== id));
+      return;
+    }
+    setEditids([...editIds, id]);
+  };
+
+  useEffect(() => {
+    const updateActLink = {
+      edit: editIds.length === 1,
+      delete: editIds.length > 0,
+    };
+
+    setActLink(updateActLink);
+  }, [editIds]);
+
   return (
     <>
       <header>
@@ -78,25 +100,33 @@ const TransactionsTable = ({ authId = "67c0ffcf02a6253bfbd4cdbb" }) => {
           <thead>
             <tr>
               <td colSpan="6" className="header-table">
-                <Link to="" className="table-action-link">
+                <Link
+                  to=""
+                  className="table-action-link"
+                  style={{ color: actLink.edit ? "#000000de" : "#a9a9a9de" }}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="16px"
                     viewBox="0 -960 960 960"
                     width="16px"
-                    fill="#5f6368"
+                    style={{ fill: "currentColor" }}
                   >
                     <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
                   </svg>
                   Edit
                 </Link>
-                <Link to="" className="table-action-link">
+                <Link
+                  to=""
+                  className="table-action-link"
+                  style={{ color: actLink.delete ? "#000000de" : "#a9a9a9de" }}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="16px"
                     viewBox="0 -960 960 960"
                     width="16px"
-                    fill="#5f6368"
+                    style={{ fill: "currentColor" }}
                   >
                     <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
                   </svg>
@@ -130,7 +160,11 @@ const TransactionsTable = ({ authId = "67c0ffcf02a6253bfbd4cdbb" }) => {
               currentRows.map((transaction, index) => (
                 <tr key={index}>
                   <td>
-                    <input type="checkbox" />
+                    <input
+                      type="checkbox"
+                      value={transaction._id}
+                      onClick={(e) => handleCheck(e.target.value)}
+                    />
                   </td>
                   <td>{transaction.category}</td>
                   <td>{format(new Date(transaction.date), "MM-dd-yyyy")}</td>
