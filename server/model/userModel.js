7e41a56feb6c8ea68ea323b5aacac0a83e7e52bb
Apples <junyiappleli@gmail.com>
2025-03-08 00:13:01 -0500

    changes

---
+++ server/model/userModel.js
@@ -0,0 +1,26 @@
+const { required } = require("nodemon/lib/config");
+
+const mongoose = requrie("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
