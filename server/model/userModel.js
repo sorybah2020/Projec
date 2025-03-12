7430e07a37d0590733f3db6f6d9179a6f5ff6811
Apples <junyiappleli@gmail.com>
2025-03-12 17:38:25 -0400

    finished user auth backend

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -22,6 +22,7 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+//Hashes the password before saving the user to the database
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
@@ -30,6 +31,12 @@ userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+//Compares the password entered by the user with the password stored in the database
+//Returns true if the passwords match, otherwise returns false
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 export default User;
