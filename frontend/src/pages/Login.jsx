54028d598f5f3e9681f8205def3c5d1bbbc65b42
Dorisa Shehi <doris.shehi2@gmail.com>
2025-03-26 13:40:02 -0400

    Validation for login form added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,32 @@
-const Login = () =>{
-
-        return (
-        <>
-            <div className="container">
-                <div id="login-form" className="form-container active">
-                    <h2>Login</h2>
-                    <input type="text" id="login-username" placeholder="Username" required/>
-                    <input type="password" id="login-password" placeholder="Password" required/>
-                    <button onclick="login()">Login</button>
-                    <p>Don't have an account? <a href="#" onclick="showSignup()">Sign Up</a></p>
-                </div>
-
-            
-            </div>
-
-        
-        
-        </>
-        )
-        
-}
+const Login = () => {
+  return (
+    <>
+      <div className="container">
+        <div id="login-form" className="form-container active">
+          <h2>Login</h2>
+          <input
+            type="text"
+            id="login-username"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            id="login-password"
+            placeholder="Password"
+            required
+          />
+          <button onclick="login()">Login</button>
+          <p>
+            Don't have an account?{" "}
+            <a href="#" onclick="showSignup()">
+              Sign Up
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,25 +1,81 @@
-const Signup = () =>{
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-    return (
+const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState({
+    signupUsername: "",
+    signupEmail: "",
+    signupPassword: "",
+  });
+
+  const reqFields = ["signup-username", "signup-email", "signup-password"];
+
+  const validateAll = (formData) => {
+    let newErrors = {};
+    let valid = true;
+
+    reqFields.forEach((field) => {
+      if (!formData[field]) {
+        newErrors[field] = "Required field.";
+        valid = false;
+      }
+    });
+
+    return { valid, newErrors };
+  };
+  const handleChange = (input) => {
+    const { name, value } = input;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { valid, newErrors } = validateAll(formData);
+    setErrors(newErrors);
+    console.log(valid, newErrors);
+  };
+  return (
     <>
-        <div className="container">
-    
-            <div id="signup-form" className="form-container">
-                <h2>Sign Up</h2>
-                <input type="text" id="signup-username" placeholder="Username" required/>
-                <input type="email" id="signup-email" placeholder="Email" required/>
-                <input type="password" id="signup-password" placeholder="Password" required/>
-                <button onclick="signup()">Sign Up</button>
-                <p>Already have an account? <a href="#" onclick="showLogin()">Login</a></p>
-            </div>
+      <div className="container">
+        <div id="signup-form" className="form-container">
+          <h2>Sign Up</h2>
+          <input
+            type="text"
+            id="signup-username"
+            placeholder="Username"
+            required
+            name="signupUsername"
+            onChange={(e) => handleChange(e.target)}
+          />
+          <input
+            type="email"
+            id="signup-email"
+            name="signupEmail"
+            placeholder="Email"
+            required
+            onChange={(e) => handleChange(e.target)}
+          />
+          <input
+            type="password"
+            id="signupPassword"
+            placeholder="Password"
+            required
+            name="signupPassword"
+            onChange={(e) => handleChange(e.target)}
+          />
+          <button onClick={(e) => handleLogin(e)}>Sign Up</button>
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
         </div>
-
-    
-    
+      </div>
     </>
-    )
-    
-}
-
+  );
+};
 
 export default Signup;
