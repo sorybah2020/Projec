40adabc0bfcda174e0e9bf46d9f965a38769d7a1
Dorisa Shehi <doris.shehi2@gmail.com>
2025-03-26 12:42:11 -0400

    Transactions empty warning added

--- server/controllers/TransactionsController.js
+++ server/controllers/TransactionsController.js
@@ -50,9 +50,10 @@ const getTransactionsById = async (req, res) => {
   try {
     const userId = req.params.authId;
     const transactions = await TransactionsModel.find({ userId: userId });
-    if (transactions.length > 0) {
-      return res.status(200).json(transactions);
+    if (transactions.length == 0) {
+      return res.status(200).json({ message: "No transactions found" });
     }
+    return res.status(200).json(transactions);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
