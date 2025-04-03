260b1eb0e7ee601791440420a52e0ac3190cb318
Dorisa Shehi <doris.shehi2@gmail.com>
2025-04-03 09:13:34 -0400

    Login/ Signup responsivity added for mobile

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -67,81 +67,84 @@ const Login = () => {
           <Logo />
         </div>
       </nav>
-      <div className="hero">
-        <div></div>
-        <div className="hero-container">
-          <div className="hero-text">
-            <div className="hero-text-container">
-              <h1>Track your finances easily</h1>
-              <p className="hero-description">
-                E-tracker helps you to track your expenses and manage your
-                finances efficiently. Log in to see if your pig is getting rich!
-              </p>
+      <main>
+        <div className="hero">
+          <div></div>
+          <div className="hero-container">
+            <div className="hero-text">
+              <div className="hero-text-container">
+                <h1>Track your finances easily</h1>
+                <p className="hero-description">
+                  E-tracker helps you to track your expenses and manage your
+                  finances efficiently. Log in to see if your pig is getting
+                  rich!
+                </p>
+              </div>
             </div>
-          </div>
-          <div id="signup-form" className="form-container">
-            <div className="form-header">
-              <h2 className="form-header">Welcome</h2>
-              <p>Log in to your E-tracker account</p>
-            </div>
+            <div id="signup-form" className="form-container">
+              <div className="form-header">
+                <h2 className="form-header">Welcome</h2>
+                <p>Log in to your E-tracker account</p>
+              </div>
 
-            <form>
-              <div className="form-group">
-                <label className="header">
-                  Email <em className="text-redText">*</em>
-                </label>
-                <div className="form-group-container diff">
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    required
-                    onChange={(e) => handleChange(e.target)}
-                  />
-                  {errors?.["email"] && (
-                    <em className="err-message">{errors["email"]}</em>
+              <form>
+                <div className="form-group">
+                  <label className="header">
+                    Email <em className="text-redText">*</em>
+                  </label>
+                  <div className="form-group-container diff">
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="Email"
+                      required
+                      onChange={(e) => handleChange(e.target)}
+                    />
+                    {errors?.["email"] && (
+                      <em className="err-message">{errors["email"]}</em>
+                    )}
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label className="header">
+                    Password <em className="text-redText">*</em>
+                  </label>
+                  <div className="form-group-container diff">
+                    <input
+                      type="password"
+                      id="password"
+                      placeholder="Password"
+                      required
+                      name="password"
+                      onChange={(e) => handleChange(e.target)}
+                    />
+                    {errors?.["password"] && (
+                      <em className="err-message">{errors["password"]}</em>
+                    )}
+                  </div>
+                </div>
+                <div className="form-group">
+                  {errors?.["frm_subms"] && (
+                    <em className="err-message">{errors["frm_subms"]}</em>
                   )}
                 </div>
-              </div>
 
-              <div className="form-group">
-                <label className="header">
-                  Password <em className="text-redText">*</em>
-                </label>
-                <div className="form-group-container diff">
-                  <input
-                    type="password"
-                    id="password"
-                    placeholder="Password"
-                    required
-                    name="password"
-                    onChange={(e) => handleChange(e.target)}
-                  />
-                  {errors?.["password"] && (
-                    <em className="err-message">{errors["password"]}</em>
-                  )}
-                </div>
-              </div>
-              <div className="form-group">
-                {errors?.["frm_subms"] && (
-                  <em className="err-message">{errors["frm_subms"]}</em>
-                )}
-              </div>
-
-              <button className="btn" onClick={(e) => handleLogin(e)}>
-                Login
-              </button>
-              <p className="more-info">
-                {"Don't have an account?"}{" "}
-                <Link to="/signup" className="sign-up-link">
-                  Sign Up
-                </Link>
-              </p>
-            </form>
+                <button className="btn" onClick={(e) => handleLogin(e)}>
+                  Login
+                </button>
+                <p className="more-info">
+                  {"Don't have an account?"}{" "}
+                  <Link to="/signup" className="sign-up-link">
+                    Sign Up
+                  </Link>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
       <Footer />
     </>
   );
