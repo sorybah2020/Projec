9591b44ea796756f34072708535e551e03282961
Apples <junyiappleli@gmail.com>
2025-04-28 12:35:50 -0400

    adding route for multiple add transactions

--- server/controllers/TransactionsController.js
+++ server/controllers/TransactionsController.js
@@ -93,6 +93,20 @@ const createTransaction = async (req, res) => {
   }
 };
 
+//for plaid api fetch
+const createMultipleTransactions = async (req, res) => {
+  const multipleTransactions = req.body.expenses;
+
+  try {
+    const newMultipleTransactions = await TransactionsModel.insertMany(
+      multipleTransactions
+    );
+  } catch (error) {
+    console.error("Error creating multiple transactions:", error);
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 const getTransactionsById = async (req, res) => {
   try {
     const userId = req.params.authId;
@@ -235,6 +249,7 @@ const deleteTransactions = async (req, res) => {
 
 export default {
   createTransaction,
+  createMultipleTransactions,
   getTransactionsById,
   getTransactionById,
   editTransaction,
